96e415f2cb9c0e4c05f7043288a4a44645fcf3b2
Pedro Vinicius Mota Barreiro <pedromb.dev@gmail.com>
2023-04-28 09:09:01 -0300

    Adicionando services do token e do user

--- Library-System/Services/TokenServices.cs
+++ Library-System/Services/TokenServices.cs
@@ -1,6 +1,32 @@
-﻿namespace Library_System.Services
+﻿using Library_System.Models;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Library_System.Services
 {
     public class TokenServices
     {
+        public string GenerateToken(UserModel user)
+        {
+            Claim[] claims = new Claim[]
+            {
+                new Claim("username", user.UserName),
+                new Claim("id", user.Id),
+                new Claim("email", user.Email),
+                new Claim(ClaimTypes.DateOfBirth, user.Birthday.ToString()),
+                new Claim("LoginTimeStamp", DateTime.UtcNow.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ASDASDASIYDAOSDOASIDVYOT131ASD"));
+            
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(expires: DateTime.Now.AddMinutes(45), claims: claims, signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
--- Library-System/Services/UserService.cs
+++ Library-System/Services/UserService.cs
@@ -1,6 +1,48 @@
-﻿namespace Library_System.Services
+﻿using AutoMapper;
+using Library_System.Data.Dtos;
+using Library_System.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Library_System.Services
 {
     public class UserService
     {
+        private readonly IMapper _mapper;
+        private readonly UserManager<UserModel> _userManager;
+        private readonly SignInManager<UserModel> _signInManager;
+        private TokenServices _tokenServices;
+
+        public UserService(IMapper mapper, UserManager<UserModel> userManager, SignInManager<UserModel> signInManager, TokenServices tokenService)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenServices = tokenService;
+        }
+
+        public async Task Create(CreateUserDto dto)
+        {
+            UserModel user = _mapper.Map<UserModel>(dto);
+
+            IdentityResult result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded) throw new ApplicationException("Falha ao cadastrar o usuario");
+        }
+
+        public async Task<string> Login(LoginUserDto dto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(dto.Username, dto.Password, false, false);
+
+            if(!result.Succeeded) throw new ApplicationException("Usuario não autenticado");
+
+            var user = _signInManager
+                .UserManager
+                .Users
+                .FirstOrDefault(user => user.NormalizedUserName == dto.Username.ToUpper());
+
+            var token = _tokenServices.GenerateToken(user);
+
+            return token;
+        }
     }
 }
